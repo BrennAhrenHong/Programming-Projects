f8ec36068082f6c8657422d41f50def55aa67e63
LeCygnus <43977429+LeCygnus@users.noreply.github.com>
2018-11-23 09:38:22 +0800

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_project.Properties
+{
+    class Person
+    {
+        /*
+             * Person class:
+             * Fields: string: FirstName, MiddleInitial, LastName, Birthdate, and Address.
+             * Empty Methods: string: GetFullName and int: GetAge.
+         */
+
+        public string FirstName;
+        public string MiddleInitial;
+        public string LastName;
+        public string Birthdate;
+        public string Address;
+
+        public string GetFullName()
+        {
+            return "Empty";
+        }
+
+        public int GetAge()
+        {
+            return 0;
+        }
+
+
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_project.Properties
+{
+    class Product
+    {
+        public string Name;
+        public string Description;
+        public string Manufacturer;
+        public int Items;
+        public decimal Price;
+
+        public void DeductItems(int items)
+        {
+
+        }
+
+    }
+}
